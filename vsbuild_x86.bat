fce0e43a096e7b29f1ac7b4eddaba5d863c997ba
COMP Omega <compomega@tutanota.com>
2016-10-16 04:50:26 -0600

    [FIX] This will keep the configurations from showing up before the first build.

--- vsbuild_x86.bat
+++ vsbuild_x86.bat
@@ -2,5 +2,5 @@
 mkdir build32 > nul 2> nul
 cd build32
 set CMAKE_PREFIX_PATH=C:\Qt\Qt5.7.0\5.7\msvc2015_32
-cmake -G"Visual Studio 14 2015" ..
+cmake -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -G"Visual Studio 14 2015" ..
 pause
