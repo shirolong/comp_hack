f79fe01a12e7fe819bf18f09b572b3844932678f
COMP Omega <compomega@tutanota.com>
2016-10-16 05:39:39 -0600

    [FIX] Use default Qt paths from the online installer.

--- vsbuild_x64.bat
+++ vsbuild_x64.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir build64 > nul 2> nul
 cd build64
-set CMAKE_PREFIX_PATH=C:\Qt\Qt5.7.0\5.7\msvc2015_64
+set CMAKE_PREFIX_PATH=C:\Qt\5.7\msvc2015_64
 cmake -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -G"Visual Studio 14 2015 Win64" ..
 pause
--- vsbuild_x86.bat
+++ vsbuild_x86.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir build32 > nul 2> nul
 cd build32
-set CMAKE_PREFIX_PATH=C:\Qt\Qt5.7.0\5.7\msvc2015_32
+set CMAKE_PREFIX_PATH=C:\Qt\5.7\msvc2015
 cmake -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -G"Visual Studio 14 2015" ..
 pause
