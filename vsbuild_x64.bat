a18c0f5204bcda4eb029674a5032c43af96a67a7
COMP Omega <compomega@tutanota.com>
2016-10-16 04:47:17 -0600

    [FIX] Debug and Release versions of external projects (for VS). It is recommended to clear out your build directory.

--- vsbuild_x64.bat
+++ vsbuild_x64.bat
@@ -1,5 +1,6 @@
 @echo off
 mkdir build64 > nul 2> nul
 cd build64
+set CMAKE_PREFIX_PATH=C:\Qt\Qt5.7.0\5.7\msvc2015_64
 cmake -G"Visual Studio 14 2015 Win64" ..
 pause
--- vsbuild_x86.bat
+++ vsbuild_x86.bat
@@ -1,5 +1,6 @@
 @echo off
 mkdir build32 > nul 2> nul
 cd build32
+set CMAKE_PREFIX_PATH=C:\Qt\Qt5.7.0\5.7\msvc2015_32
 cmake -G"Visual Studio 14 2015" ..
 pause
