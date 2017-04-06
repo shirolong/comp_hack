c6e7b76317e09f61d9890441c26236e3f292ae8d
HikaruM <HikaruM@users.noreply.github.com>
2017-04-06 16:37:21 -0700

    Updated say chat radius to be significantly larger. (#182)
    * Updated say chat radius to be significantly larger.
    
    * Updated:
    
    Range set to 8,000.
    To do comment removed.

--- libcomp/src/Constants.h
+++ libcomp/src/Constants.h
@@ -119,8 +119,8 @@ namespace libcomp
 /// Chat message is visible to all GMs.
 #define CHAT_VISIBILITY_GMS    (7)
 
-// Chat Radius for Say Chat.  ToDo: Define Proper Range.
-#define CHAT_RADIUS_SAY (92)
+// Chat Radius for Say Chat.
+#define CHAT_RADIUS_SAY (8000)
 
 /// Number of G1 times stored.
 #define G1_TIME_COUNT (18)
