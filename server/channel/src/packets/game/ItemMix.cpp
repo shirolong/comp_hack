395c267961e16f7bd866819162b63a6f0bb425ec
HACKfrost <HACKfrost@users.noreply.github.com>
2019-11-12 17:24:45 -0800

    ItemMix great success crash fix redux (#1094)

--- server/channel/src/packets/game/ItemMix.cpp
+++ server/channel/src/packets/game/ItemMix.cpp
@@ -317,13 +317,14 @@ bool Parsers::ItemMix::Parse(libcomp::ManagerPacket *pPacketManager,
         auto outItem2 = blendData->GetResultItems(1);
 
         uint32_t item1Type = outItem1->GetItemID();
-        uint32_t item2Type = outItem2 ? outItem2->GetItemID() : 0;
+        uint32_t item2Type = outItem2->GetItemID() != static_cast<uint32_t>(-1)
+            ? outItem2->GetItemID() : 0;
 
         uint16_t item1Min = outItem1->GetMin();
-        uint16_t item2Min = outItem2 ? outItem2->GetMin() : 0;
+        uint16_t item2Min = item2Type ? outItem2->GetMin() : 0;
 
         uint16_t item1Max = outItem1->GetMax();
-        uint16_t item2Max = outItem2 ? outItem2->GetMax() : 0;
+        uint16_t item2Max = item2Type ? outItem2->GetMax() : 0;
 
         // Apply extension transformations
         if(extItemDefs.size() > 0)
